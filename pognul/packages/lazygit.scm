4640678a2096a9142758ffa72a9fa5ea2e7cb3b2
Nikita Mitasov <mitanick@ya.ru>
2025-06-12 16:49:23 +0300

    lazygit: syntax change, fmt

--- pognul/packages/lazygit.scm
+++ pognul/packages/lazygit.scm
@@ -1,27 +1,32 @@
 (define-module (pognul packages lazygit)
   #:use-module (guix packages)
   #:use-module (guix build-system go)
-  #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (gnu packages golang)
-  #:use-module (guix licenses))
+  #:use-module ((guix licenses)
+                #:prefix license:))
 
 (define-public lazygit
   (package
-    (name "lazygit")
-    (version "0.52.0")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/jesseduffield/lazygit/archive/refs/tags/v" version ".tar.gz"))
-              (sha256 (base32 "1y363yn884d1mshzgdnwf6dgjw5b9f1si7zam6s6z8yc0m8h8srd"))
-              (file-name (git-file-name name version))))
-    (build-system go-build-system)
-    (arguments
-     (list #:import-path "github.com/jesseduffield/lazygit"
-	   #:go go-1.24
-           #:tests? #f
-           #:install-source? #f))
-    (home-page "https://github.com/jesseduffield/lazygit")
-    (synopsis "Simple terminal UI for git commands")
-    (description "Simple terminal UI for git commands")
-    (license expat)))
+   (name "lazygit")
+   (version "0.52.0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/jesseduffield/lazygit")
+           (commit (string-append "v" version))))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "0nijsnx9nq8kdmjcx9g2fxbj6rmx2wqy1xr5aysgzlc1ysi53cdm"))))
+   (build-system go-build-system)
+   (arguments
+    (list
+     #:import-path "github.com/jesseduffield/lazygit"
+     #:go go-1.24
+     #:tests? #f
+     #:install-source? #f))
+   (home-page "https://github.com/jesseduffield/lazygit")
+   (synopsis "Simple terminal UI for git commands")
+   (description "Simple terminal UI for git commands")
+   (license license:expat)))
