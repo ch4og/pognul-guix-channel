3545dac12df99c48965676ebae2dc2a8158b6080
Nikita Mitasov <me@ch4og.com>
2025-07-28 09:13:45 +0300

    lazygit: switch to nix-go build system

--- pognul/packages/lazygit.scm
+++ pognul/packages/lazygit.scm
@@ -3,7 +3,7 @@
 
 (define-module (pognul packages lazygit)
   #:use-module (guix packages)
-  #:use-module (guix build-system go)
+  #:use-module (pognul build-system nix-go)
   #:use-module (guix git-download)
   #:use-module (gnu packages golang)
   #:use-module (gnu packages version-control)
@@ -23,13 +23,10 @@
        (file-name (git-file-name name version))
        (sha256
         (base32 "0nijsnx9nq8kdmjcx9g2fxbj6rmx2wqy1xr5aysgzlc1ysi53cdm"))))
-    (build-system go-build-system)
+    (build-system nix-go-build-system)
     (arguments
-     (list
-      #:import-path "github.com/jesseduffield/lazygit"
-      #:go go-1.24
-      #:tests? #f
-      #:install-source? #f))
+     `(#:vendor-hash "07q05pbl11r39z0qlqfs70xvzxb5nj5288vkhb6sj1irngcx9767"
+       #:go ,go-1.24))
     (propagated-inputs (list git-minimal))
     (home-page "https://github.com/jesseduffield/lazygit")
     (synopsis "Simple terminal UI for git commands")
